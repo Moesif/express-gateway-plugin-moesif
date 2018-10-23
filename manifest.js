c552233093f42009c47fc94b0912b1d5bfe38e2e
xinghengwang <xing@moesif.com>
2018-10-23 12:42:17 -0700

    Feature: added support for identifyUser and sessionToken.

--- manifest.js
+++ manifest.js
@@ -1,28 +1,37 @@
-
+'use strict';
 var createMiddleware = require('./middlewareCreator');
 
 module.exports = {
   version: '1.3.0',
   init: function(pluginContext) {
-    console.log('hello you there');
-    console.log(pluginContext);
-
     var middleWare = createMiddleware(pluginContext);
 
     pluginContext.registerPolicy({
       name: 'moesif',
       policy: (actionParams) => {
-        console.log('got action params');
-        console.log(actionParams);
-
         return middleWare;
       }
     });
-    console.log(pluginContext);
+    // console.log(pluginContext);
   },
   policies: ['moesif'], // this is for CLI to automatically add to "policies" whitelist in gateway.config
   schema: {
     // This is for CLI to ask about params 'eg plugin configure example'
-    $id: 'http://www.moesif.com/schemas/policies/moesif.json'
+    "$id": 'http://www.moesif.com/schemas/policies/moesif.json',
+    "type": 'object',
+    "properties": {
+      applicationId: {
+        type: 'string',
+        description: 'application id obtained from your moesif account.'
+      },
+      sessionTokenHeader: {
+        type: 'string',
+        description: 'the header used for session tokens.'
+      },
+      debug: {
+        type: 'boolean'
+      }
+    },
+    required: [ 'applicationId' ]
   }
 };
--- middlewareCreator.js
+++ middlewareCreator.js
@@ -1,18 +1,62 @@
+'use strict';
+
 var moesifExpress = require('moesif-express');
 var safeGet = require('lodash/get');
 
-
 module.exports = function(pluginContext) {
-
-
   var applicationId = safeGet(pluginContext, 'settings.applicationId');
+  var debug = safeGet(pluginContext, 'settings.debug');
+  var tokenHeader = safeGet(pluginContext, 'settings.sessionTokenHeader');
 
-  console.log('got application Id back from pluginContext: ' + applicationId);
+  if (debug) {
+    console.log('debug mode is on');
+    console.log('got application Id back from pluginContext: ' + applicationId);
+    console.log('tokenHeader ' + tokenHeader);
+  }
+
+  if (!applicationId) {
+    throw new Error('moesif plugin requires application Id to be defined');
+  }
+
+  function identifyUser(req, res) {
+    // Express Gateway will add the eg-consumer-id header to
+    // the proxied requests based on the authenticated user. In
+    // case there’s none, its value will be anonymous.
+    if (debug) {
+      console.log('identify user triggered');
+    }
+    var userId = safeGet(req, 'headers.eg-consumer-id');
+    if (debug) {
+      console.log(JSON.stringify(req.headers));
+      console.log('userid is ' + userId);
+    }
+    return userId;
+  }
+
+  var getSessionToken;
+
+  if (tokenHeader) {
+    getSessionToken = function(req, res) {
+      if (debug) {
+        console.log('getSessionToken is triggered');
+      }
+      var headers = safeGet(req, 'headers');
+      if (!headers) return null;
+      var obtainedToken = headers[tokenHeader] || headers[tokenHeader.toLowerCase()] || headers['authorization'] || headers['Authorization'];
+
+      if (debug) {
+        console.log('obtained token ' + obtainedToken);
+      }
+      return obtainedToken;
+    }
+  }
 
   var moesifMiddleware = moesifExpress({
     applicationId: applicationId,
-    debug: true
+    debug: debug,
+    identifyUser: identifyUser,
+    getSessionToken: getSessionToken
   });
 
   return moesifMiddleware;
-}
+};
